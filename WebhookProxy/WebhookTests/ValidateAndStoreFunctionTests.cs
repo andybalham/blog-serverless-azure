63f638e60f86aea649ae07847540f4a77d2e9648
Andy Blackledge <andyblackledge@hotmail.com>
2024-03-02 10:06:53 +0000

    Created unit tests

--- WebhookProxy/WebhookTests/ValidateAndStoreFunctionTests.cs
+++ WebhookProxy/WebhookTests/ValidateAndStoreFunctionTests.cs
@@ -13,30 +13,43 @@ namespace WebhookTests
 {
     public class ValidateAndStoreFunctionTests
     {
-        [Fact]
-        public void ValidRequest_CreatedResponseAndRequestStored()
-        {
-            // Arrange
+        private Mock<ILoggerFactory> _mockLoggerFactory;
+        private Mock<IRequestValidator> _mockRequestValidator;
 
-            var mockLoggerFactory = new Mock<ILoggerFactory>();
-            mockLoggerFactory.Setup(lf => lf
+        public ValidateAndStoreFunctionTests()
+        {
+            _mockLoggerFactory = new Mock<ILoggerFactory>();
+            _mockLoggerFactory.Setup(lf => lf
                 .CreateLogger(It.IsAny<string>()))
                 .Returns(new Mock<ILogger>().Object);
 
-            var mockRequestValidator = new Mock<IRequestValidator>();
-            mockRequestValidator.Setup(rv => rv
+            _mockRequestValidator = new Mock<IRequestValidator>();
+        }
+
+        [Fact]
+        public void ValidRequest_CreatedResponse_StoredAsValid()
+        {
+            // Arrange
+
+            const string ExpectedContractId = "contractId";
+            const string ExpectedSenderId = "senderId";
+            const string ExpectedTenantId = "tenantId";
+
+            var requestValidationResult = new RequestValidationResult { IsValid = true };
+
+            _mockRequestValidator.Setup(rv => rv
                 .Validate(It.IsAny<string>(), It.IsAny<string>()))
-                .Returns(new RequestValidationResult { IsValid = true });
+                .Returns(requestValidationResult);
 
             var mockRequestStore = new Mock<IRequestStore>();
 
             var validateAndStoreSUT = new ValidateAndStoreFunction(
-                mockLoggerFactory.Object, mockRequestValidator.Object, mockRequestStore.Object);
+                _mockLoggerFactory.Object, _mockRequestValidator.Object, mockRequestStore.Object);
 
             // Act
 
             var response = 
-                validateAndStoreSUT.Run(new MockHttpRequestData(new { }), "contractId", "senderId", "tenantId");
+                validateAndStoreSUT.Run(new MockHttpRequestData(new { }), ExpectedContractId, ExpectedSenderId, ExpectedTenantId);
 
             // Assert
 
@@ -44,7 +57,57 @@ namespace WebhookTests
 
             response.StatusCode.Should().Be(HttpStatusCode.Created);
 
-            // TODO: Assert the response is stored
+            mockRequestStore.Verify(rs => 
+                rs.PutValidRequest(
+                    It.IsAny<HttpRequestData>(), 
+                    It.Is<string>(p => p == ExpectedContractId), 
+                    It.Is<string>(p => p == ExpectedSenderId), 
+                    It.Is<string>(p => p == ExpectedTenantId)));
+        }
+
+        [Fact]
+        public void InvalidRequest_BadRequestResponseWithErrors_StoredAsInvalid()
+        {
+            // Arrange
+
+            const string ExpectedContractId = "contractId";
+            const string ExpectedSenderId = "senderId";
+            const string ExpectedTenantId = "tenantId";
+
+            var requestValidationResult = 
+                new RequestValidationResult 
+                { 
+                    IsValid = false,
+                    ErrorMessages = new List<string> { "Missing properties" }
+                };
+
+            _mockRequestValidator.Setup(rv => rv
+                .Validate(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(requestValidationResult);
+
+            var mockRequestStore = new Mock<IRequestStore>();
+
+            var validateAndStoreSUT = new ValidateAndStoreFunction(
+                _mockLoggerFactory.Object, _mockRequestValidator.Object, mockRequestStore.Object);
+
+            // Act
+
+            var response =
+                validateAndStoreSUT.Run(new MockHttpRequestData(new { }), ExpectedContractId, ExpectedSenderId, ExpectedTenantId);
+
+            // Assert
+
+            response.Should().NotBeNull();
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+            mockRequestStore.Verify(rs =>
+                rs.PutInvalidRequest(
+                    It.IsAny<HttpRequestData>(),
+                    It.Is<string>(p => p == ExpectedContractId),
+                    It.Is<string>(p => p == ExpectedSenderId),
+                    It.Is<string>(p => p == ExpectedTenantId),
+                    It.Is<IList<string>>(p => p == requestValidationResult.ErrorMessages)));
         }
     }
 }
